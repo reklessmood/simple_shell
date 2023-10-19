3f468d6a807ab9773c2591424dd968f2cb7bbe7b
reklessmood <nizarbellamqaddam@gmail.com>
2023-10-19 20:42:29 +0100

    fix

--- aliaschain.c
+++ aliaschain.c
@@ -45,13 +45,15 @@ int is_chain(info_t *info, char *buf, size_t *p)
 void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len) {
 	size_t j = *p;
 
-	if (info->cmd_buf_type == CMD_AND) {
+	if (info->cmd_buf_type == CMD_AND)
+	{
 		if (info->status) {
 			buf[i] = 0;
 			j = len;
 		}
 	}
-	if (info->cmd_buf_type == CMD_OR) {
+	if (info->cmd_buf_type == CMD_OR)
+	{
 		if (!info->status) {
 			buf[i] = 0;
 			j = len;
@@ -72,7 +74,8 @@ int replace_alias(info_t *info) {
 	list_t *node;
 	char *p;
 
-	for (i = 0; i < 10; i++) {
+	for (i = 0; i < 10; i++)
+	{
 		node = node_starts_with(info->alias, info->argv[0], '=');
 		if (!node) {
 			return 0;
@@ -101,15 +104,18 @@ int replace_vars(info_t *info) {
 	int i = 0;
 	list_t *node;
 
-	for (i = 0; info->argv[i]; i++) {
+	for (i = 0; info->argv[i]; i++)
+	{
 		if (info->argv[i][0] != '$' || !info->argv[i][1]) {
 			continue;
 		}
 
-		if (!_strcmp(info->argv[i], "$?")) {
+		if (!_strcmp(info->argv[i], "$?"))
+		{
 			replace_string(&(info->argv[i]), _strdup(convert_number(info->status, 10, 0)));
 			continue;}
-		if (!_strcmp(info->argv[i], "$$")) {
+		if (!_strcmp(info->argv[i], "$$"))
+		{
 			replace_string(&(info->argv[i]), _strdup(convert_number(getpid(), 10, 0)));
 			continue;
 		}
@@ -130,7 +136,8 @@ int replace_vars(info_t *info) {
  *
  * Return: 1 if replaced, 0 otherwise.
  */
-int replace_string(char **old, char *new) {
+int replace_string(char **old, char *new)
+{
 	free(*old);
 	*old = new;
 	return 1;
