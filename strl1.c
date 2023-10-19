6d2d6f40c3ce2c2b93e8f54f129fb754483b62eb
reklessmood <nizarbellamqaddam@gmail.com>
2023-10-19 21:28:04 +0100

    fix

--- strl1.c
+++ strl1.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+#include <unistd.h>
 
 /**
  * _strcpy - Copies a string.
@@ -46,31 +47,12 @@ char *_strdup(const char *str)
 }
 
 /**
- * _puts - Prints an input string.
- * @str: The string to be printed.
- *
- * Return: Nothing.
- */
-void _puts(char *str)
-{
-	int i = 0;
-
-	if (!str)
-		return;
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
-}
-
-/**
  * _putchar - Writes the character c to stdout.
  * @c: The character to print.
  *
  * Return: On success 1. On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+int putchar(char c)
 {
 	static int i;
 	static char buf[WRITE_BUF_SIZE];
@@ -83,4 +65,23 @@ int _putchar(char c)
 	if (c != BUF_FLUSH)
 		buf[i++] = c;
 	return 1;
+}
+
+/**
+ * _puts - Prints an input string.
+ * @str: The string to be printed.
+ *
+ * Return: Nothing.
+ */
+void puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i++;
+	}
 }
